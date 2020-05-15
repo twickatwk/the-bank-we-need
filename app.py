b5f34af601e05633be9bd5bba555192180bfe22f
Daryln Too <darlyngtoo@gmail.com>
2020-05-15 22:25:33 +0800

    Adding db code

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+
+#Database
+app.config.from_pyfile('config.cfg')
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    user_name = db.Column(db.String(255), primary_key=True, nullable=False, unique=True)
+    user_id = db.Column(db.Integer, nullable=False, unique=True)
+    first_name = db.Column(db.String(255), unique=False)
+    last_name = db.Column(db.String(255), unique=False)
 
 @app.route('/')
 def home_page():
@@ -17,3 +30,7 @@ def loan_page():
 @app.route('/crowdsourcing')
 def crowdsourcing():
     return render_template('crowdsourcing.html')
+
+
+if __name__ == '__main__':
+    app.run()
