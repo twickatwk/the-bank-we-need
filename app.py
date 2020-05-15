4168a290da0579771572370be9eb6b195a06b27a
unknown <atwkdeveloper@outlook.com>
2020-05-15 15:17:09 +0800

    added routing for flask app

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home_page():
+    return render_template("index.html")
+
+@app.route('/grants')
+def grant_page():
+    return render_template('grants.html')
+
+@app.route('/loans')
+def loan_page():
+    return render_template('loans.html')
+
+@app.route('/crowdsourcing')
+def crowdsourcing():
+    return render_template('crowdsourcing.html')
